4cb7d94a842c8065b61a2962f07085f7dd2870d9
Pranitha2810 <pranithanuvvula5@gmail.com>
2025-02-04 02:24:38 +0530

    Time: 3 ms (63.2%), Space: 18.4 MB (5.42%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -0,0 +1,10 @@
+class Solution:
+    def duplicateZeros(self, nums: List[int]) -> None:
+        i=0
+        while i<len(nums):
+            if nums[i]==0:
+                nums.insert(i+1,0)
+                nums.pop()
+                i+=2
+            else:
+                i+=1     
