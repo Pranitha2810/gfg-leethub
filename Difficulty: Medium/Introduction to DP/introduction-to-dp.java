b75257b4926caf5da2144652f01f2ce2eb91bdd7
Pranitha2810 <pranithanuvvula5@gmail.com>
2025-02-04 19:29:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Introduction to DP/introduction-to-dp.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            Solution obj = new Solution();
+            long topDownans = obj.topDown(n);
+            long bottomUpans = obj.bottomUp(n);
+            if (topDownans != bottomUpans)
+                System.out.println(-1);
+            else
+                System.out.println(topDownans);
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+     static final int MOD = 1_000_000_007; 
+    static long topDown(int n) {
+        // code here
+        //memiozation
+        long[] dp = new long[n+1];
+        Arrays.fill(dp,-1);
+        return memo(n,dp);
+    }
+    static long memo(int n, long[] dp)
+    {
+        if(n<=1)
+            return (long)n;
+        if(dp[n] != -1)
+            return dp[n];
+        return dp[n]=(memo(n-1,dp)+memo(n-2,dp))%MOD;
+    }
+
+    static long bottomUp(int n) {
+        // code here
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        
+        long[] dp = new long[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2])%MOD;
+        }
+        return dp[n];
+    }
+}
