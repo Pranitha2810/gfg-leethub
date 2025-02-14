2e9d595ea7aac8bc397b148aa9caa178b7abd3af
Pranitha2810 <pranithanuvvula5@gmail.com>
2025-02-14 22:06:56 +0530

    Time: 0 ms (100%), Space: 41 MB (59.3%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,16 +2,25 @@ class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
         int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return memo(n-1,nums,dp);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            int rob=nums[i];
+            if(i>1)
+                rob+=dp[i-2];
+            int notrob=0+dp[i-1];
+            dp[i]=Math.max(rob,notrob);
+        }
+        return dp[n-1];
     }
-    static int memo(int i,int[] nums,int[] dp)
-    {
-        if(i==0) return nums[0];
-        if(i<0) return 0;
-        if(dp[i] != -1) return dp[i];
-        int rob = nums[i]+memo(i-2,nums,dp);
-        int notrob = 0+memo(i-1,nums,dp);
-        return dp[i]=Math.max(rob,notrob);
-    }
+    //memozation
+    // static int memo(int i,int[] nums,int[] dp)
+    // {
+    //     if(i==0) return nums[0];
+    //     if(i<0) return 0;
+    //     if(dp[i] != -1) return dp[i];
+    //     int rob = nums[i]+memo(i-2,nums,dp);
+    //     int notrob = 0+memo(i-1,nums,dp);
+    //     return dp[i]=Math.max(rob,notrob);
+    // }
 }
