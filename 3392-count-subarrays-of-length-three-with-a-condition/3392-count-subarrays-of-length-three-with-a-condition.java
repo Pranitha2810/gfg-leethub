b8c234d299a839f9e609729630e6344c539af866
Pranitha2810 <pranithanuvvula5@gmail.com>
2025-04-27 22:56:15 +0530

    Time: 1 ms (100%), Space: 45 MB (70.48%) - LeetHub

--- 3392-count-subarrays-of-length-three-with-a-condition/3392-count-subarrays-of-length-three-with-a-condition.java
+++ 3392-count-subarrays-of-length-three-with-a-condition/3392-count-subarrays-of-length-three-with-a-condition.java
@@ -5,7 +5,7 @@ class Solution {
             int a = nums[i];
             int b = nums[i+1];
             int c = nums[i+2];
-            if (a + c == b / 2.0) { 
+            if (a + c == b / 2.0) { // use .0 to ensure proper division
                 count++;
             }
         }
