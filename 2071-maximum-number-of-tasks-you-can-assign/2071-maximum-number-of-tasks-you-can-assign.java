d6431d883298c0e2692bd7216eae40cf7d16b608
Pranitha2810 <pranithanuvvula5@gmail.com>
2025-05-01 19:23:57 +0530

    Time: 1865 ms (19.59%), Space: 57.2 MB (8.98%) - LeetHub

--- 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.java
+++ 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.java
@@ -1,50 +1,58 @@
-import java.util.*;
-
 class Solution {
     public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
         Arrays.sort(tasks);
         Arrays.sort(workers);
-
-        int left = 0, right = Math.min(tasks.length, workers.length);
-        int ans = 0;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canAssign(tasks, workers, pills, strength, mid)) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+        
+        int low = 0;
+        int high = Math.min(tasks.length, workers.length);
+        int assigned = 0;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canAssign(mid, workers, tasks, pills, strength)) {
+                assigned = mid;
+                low = mid + 1;
+            } else
+                high = mid - 1;
         }
-
-        return ans;
+        return assigned;
     }
+    private boolean canAssign(int mid, int[] workers, int[] tasks, int pills, int strength)
+    {
+        TreeMap<Integer, Integer> usableWorkers = new TreeMap<>();
+        int n = workers.length;
+        for (int i = n - mid; i < n; i++)
+            usableWorkers.put(workers[i], usableWorkers.getOrDefault(workers[i], 0) + 1);
 
-    private boolean canAssign(int[] tasks, int[] workers, int pills, int strength, int k) {
-        Deque<Integer> availableWorkers = new ArrayDeque<>();
-        int m = workers.length;
-
-        // Take the last k workers (strongest ones)
-        for (int i = m - k; i < m; i++) {
-            availableWorkers.addLast(workers[i]);
-        }
-
-        int remainingPills = pills;
-        for (int i = k - 1; i >= 0; i--) {
+        for (int i = mid - 1; i >= 0; --i)
+        {
             int task = tasks[i];
-            if (!availableWorkers.isEmpty() && availableWorkers.peekLast() >= task) {
-                availableWorkers.pollLast();
-            }
-            else if (!availableWorkers.isEmpty() && availableWorkers.peekFirst() + strength >= task && remainingPills > 0) {
-                availableWorkers.pollFirst();
-                remainingPills--;
-            }
-            else {
-                return false;
+            Integer currWorker = usableWorkers.lastKey();
+            
+            if (currWorker < task)
+            {
+                if (pills <= 0)
+                    return false;
+                
+                // Find the weakest worker who can do the task with pill
+                Integer weakestWorker = usableWorkers.ceilingKey(task - strength);
+                if (weakestWorker == null)
+                    return false;
+                
+                pills--;
+
+                usableWorkers.put(weakestWorker, usableWorkers.get(weakestWorker) - 1);
+                if (usableWorkers.get(weakestWorker) == 0)
+                    usableWorkers.remove(weakestWorker);
+
+            } 
+            else
+            {
+                usableWorkers.put(currWorker, usableWorkers.get(currWorker) - 1);
+                if (usableWorkers.get(currWorker) == 0)
+                    usableWorkers.remove(currWorker);
             }
         }
-
         return true;
     }
 }
