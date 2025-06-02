f073987bd8970d9d297d5cdd6302e8848d1b1645
Pranitha2810 <pranithanuvvula5@gmail.com>
2025-06-02 20:08:47 +0530

    Time: 34 ms (7.87%), Space: 19.9 MB (36.16%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,25 +1,14 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         n=len(ratings)
-        summ=1
-        i=1
-        while i<n:
-            if ratings[i]==ratings[i-1]:
-                summ+=1
-                i+=1
-                continue
-            peak=1
-            while i<n and ratings[i]>ratings[i-1]:
-                peak+=1
-                summ+=peak
-                i+=1
-            down=0
-            while i<n and ratings[i]<ratings[i-1]:
-                down+=1
-                summ+=down
-                i+=1
-            if down>=peak:
-                summ+=down-peak+1
-        return summ
-        
-        
+        count=[1]*n
+        for i in range(1,n):
+            if ratings[i]>ratings[i-1]:
+                count[i]=max(count[i],count[i-1]+1)
+        for i in range(n-2,-1,-1):
+            if ratings[i]>ratings[i+1]:
+                count[i]=max(count[i],count[i+1]+1)
+        res=0
+        for i in range(n):
+            res+=max(count[i],count[i])
+        return res      
