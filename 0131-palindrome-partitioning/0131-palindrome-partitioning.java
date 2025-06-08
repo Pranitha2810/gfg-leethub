b4825f08ce12e7f407846dd6b3fdb7c1b9a73c99
Pranitha2810 <pranithanuvvula5@gmail.com>
2025-06-09 00:07:28 +0530

    Time: 7 ms (97.62%), Space: 56.9 MB (56.31%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,28 +1,34 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        backtrack(s, 0, new ArrayList<>(), result);
-        return result;
+        List<List<String>> res = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        solve(res,path,s,0);
+        return res;
     }
-
-    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
-        if (start == s.length()) {
-            result.add(new ArrayList<>(path)); // Add a copy of the current path
+    private void solve(List<List<String>> res,List<String> path,String s,int index)
+    {
+        if(index==s.length())
+        {
+            res.add(new ArrayList<>(path));
             return;
         }
-
-        for (int end = start; end < s.length(); end++) {
-            if (isPalindrome(s, start, end)) {
-                path.add(s.substring(start, end + 1));
-                backtrack(s, end + 1, path, result);
-                path.remove(path.size() - 1); // backtrack
+        for(int i=index;i<s.length();i++)
+        {
+            if(isPalindrome(s,index,i))
+            {
+                path.add(s.substring(index,i+1));
+                solve(res,path,s,i+1);
+                path.remove(path.size()-1);
             }
         }
     }
-
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left++) != s.charAt(right--)) return false;
+    private boolean isPalindrome(String s, int start,int end)
+    {
+        while(start<end)
+        {
+            if(s.charAt(start) != s.charAt(end)) return false;
+            start++;
+            end--;
         }
         return true;
     }
